6fbf8ebbd28cad91c5018d63908fb36dcebf7106
AlmViSi <aleemoketo@gmail.com>
2025-06-15 19:32:07 +0300

    1
    1

--- scripts/character.js
+++ scripts/character.js
@@ -1,19 +1,3 @@
-// Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyATqz2FEvuqJuFpeRNWs7MZ_G-jeEPk_dA",
-    authDomain: "leot-rpg.firebaseapp.com",
-    databaseURL: "https://leot-rpg-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "leot-rpg",
-    storageBucket: "leot-rpg.appspot.com",
-    messagingSenderId: "778831773412",
-    appId: "1:778831773412:web:b27c883c463e41a25d36f1",
-    measurementId: "G-P40D2VF1TQ"
-};
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-const database = firebase.database();
-
 document.addEventListener('DOMContentLoaded', () => {
     // DOM Elements
     const portraitUpload = document.getElementById('portrait-upload');
---
+++ scripts/firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyATqz2FEvuqJuFpeRNWs7MZ_G-jeEPk_dA",
+    authDomain: "leot-rpg.firebaseapp.com",
+    databaseURL: "https://leot-rpg-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "leot-rpg",
+    storageBucket: "leot-rpg.appspot.com",
+    messagingSenderId: "778831773412",
+    appId: "1:778831773412:web:b27c883c463e41a25d36f1",
+    measurementId: "G-P40D2VF1TQ"
+};
